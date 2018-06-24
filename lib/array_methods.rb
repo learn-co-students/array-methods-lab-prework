ca75ab5152046f5d4e56fc77bd68d1cc59befa71
Bryan Teng <bteng1226@gmail.com>
2018-06-24 00:53:05 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,17 +1,17 @@
 def using_include(array, element)
-
+array.include?(element)
 end
 
 def using_sort(array)
-
+array.sory
 end
 
 def using_reverse(array)
-
+array.reverse
 end
 
 def using_first(array)
-
+array.first
 end
 
 def using_last(array)
